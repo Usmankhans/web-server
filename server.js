31ebcf194fc7e6c0c1df00577bceb456aa062659
Usman <usmankhan4619@gmail.com>
2017-02-16 17:29:15 +0200

    Initialize repo

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require ('express');
+var app= express();
+var port= 3000;
+
+var middleware ={
+	requireAuthentication: function(req,res,next){
+		console.log("private rout hit");
+		next();
+	},
+	logger: function(req, res,next){
+		console.log('Date: ' + new Date().toString()+ ' Request: '+ req.method + ' '+ req.originalUrl);
+		next()
+	}
+};
+app.use(middleware.logger);
+app.get('/about', function(req, res){
+res.send('its about us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port,function(){
+	console.log('Server started baby at port '+ port);
+});
